c9dafc4737d52d8d3ce233dfbdfcf50f86e09835
Academy Valor Software <academy@valor-software.com>
2020-02-28 21:15:21 +0200

    test: ts

---
+++ test.ts
@@ -0,0 +1,65 @@
+type AnimalArr = Animal [];
+
+
+
+class Animal implements User {
+    constructor(public name: string) {
+    }
+    makeSound() {
+        console.log(this.name);
+    }
+}
+
+class Cat extends Animal {
+    constructor(name: string) {
+        super(`${name} + cat`);
+    }
+}
+
+class Dog extends Animal {
+    constructor(name: string) {
+        super(`${name} + dog`);
+    }
+}
+
+function makeAnimalSound(arrAnimal: AnimalArr ) {
+    for (const animal of arrAnimal) {
+        animal.makeSound();
+    }
+}
+
+makeAnimalSound([new Animal('AAA'), new Cat('BBB'), new Dog('CCC')]);
+
+
+interface User {
+    readonly age?: number;
+    name: string;
+}
+
+
+/*
+
+interface Uxzser {
+    lastName: string;
+}
+*/
+
+//let myUser = NumOrSrt;
+
+/*
+
+let objUser: User = { name: 'Anton' };
+
+
+objUser.name = 'test';
+*/
+
+
+/*
+function myFunction(str1: string): number  {
+    return 0;
+}
+
+
+let fun: myFun = myFunction;
+*/
