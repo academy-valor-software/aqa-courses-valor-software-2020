334938692431cd16a1b687e875c51107f93211c9
Genuine <MasalitinJ@gmail.com>
2020-03-01 17:06:47 +0200

    test.ts(test.js: comparing two types of languages

---
+++ test.js
@@ -0,0 +1,27 @@
+var MyClass = /** @class */ (function () {
+    function MyClass(name) {
+        this.name = name;
+    }
+    return MyClass;
+}());
+var foo = new MyClass('Anton');
+console.log(foo.name);
+
+
+console.log('smdkwmfkwmfkme');
+/*
+interface Uxzser {
+    lastName: string;
+}
+*/
+//let myUser = NumOrSrt;
+/*
+let objUser: User = { name: 'Anton' };
+objUser.name = 'test';
+*/
+/*
+function myFunction(str1: string): number  {
+    return 0;
+}
+let fun: myFun = myFunction;
+*/
---
+++ test.ts
@@ -0,0 +1,27 @@
+type NumOrSrt = number | string;
+class MyClass implements User {
+    constructor(public name: string) {
+    }
+}
+let foo = new MyClass('Anton');
+console.log(foo.name);
+interface User {
+    readonly age?: number;
+    name: string;
+}
+/*
+interface Uxzser {
+    lastName: string;
+}
+*/
+// let myUser = NumOrSrt;
+/*
+let objUser: User = { name: 'Anton' };
+objUser.name = 'test';
+*/
+/*
+function myFunction(str1: string): number  {
+    return 0;
+}
+let fun: myFun = myFunction;
+*/
