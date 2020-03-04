094a6e6563978b4837638ffe87af20d485998abc
Genuine <MasalitinJ@gmail.com>
2020-03-05 01:03:16 +0200

    tests(selectors): HW1

--- protractor/e2e/example.e2e-spec.ts
+++ protractor/e2e/example.e2e-spec.ts
@@ -1,13 +1,34 @@
-import {browser} from 'protractor';
+import {browser , $} from 'protractor';
 
-describe('Test', function () {
-    it('should passed', async () => {
-        await browser.get('/');
-        expect(true).toBe(true);
-    });
+// describe('Test', function () {
+//     it('should passed', async () => {
+//         browser.waitForAngularEnabled(false);
+//         await browser.get('/');
+//         $('[name="q"]).sendKeys('smdksmdmsdmsmd');
+//         await browser.get('/');
+//         expect(true).toBe(true);
+//     });
+//
+//     it('should failed', async () => {
+//         await browser.get('/');
+//         expect(true).toBe(false);
+//     });
+// });
 
-    it('should failed', async () => {
-        await browser.get('/');
-        expect(true).toBe(false);
+describe('angularjs homepage todo list', function() {
+    it('should add a todo', function() {
+        browser.get('https://angularjs.org');
+
+        element(by.model('todoList.todoText')).sendKeys('write first protractor test');
+        element(by.css('[value="add"]')).click();
+
+        var todoList = element.all(by.repeater('todo in todoList.todos'));
+        expect(todoList.count()).toEqual(3);
+        expect(todoList.get(2).getText()).toEqual('write first protractor test');
+
+        // You wrote your first test, cross it off the list
+        todoList.get(2).element(by.css('input')).click();
+        var completedAmount = element.all(by.css('.done-true'));
+        expect(completedAmount.count()).toEqual(2);
     });
 });
--- test.ts
+++ test.ts
@@ -1,24 +1,56 @@
-type NumOrSrt = number | string;
-class MyClass implements User {
+type AnimalArr = Animal [];
+
+
+
+class Animal implements User {
     constructor(public name: string) {
     }
+    makeSound() {
+        console.log(this.name);
+    }
 }
-let foo = new MyClass('Anton');
-console.log(foo.name);
+
+class Cat extends Animal {
+    constructor(name: string) {
+        super(`${name} + cat`);
+    }
+}
+
+class Dog extends Animal {
+    constructor(name: string) {
+        super(`${name} + dog`);
+    }
+}
+
+function makeAnimalSound(arrAnimal: AnimalArr ) {
+    for (const animal of arrAnimal) {
+        animal.makeSound();
+    }
+}
+
+makeAnimalSound([new Animal('AAA'), new Cat('BBB'), new Dog('CCC')]);
+
+
 interface User {
     readonly age?: number;
     name: string;
 }
+
+
 /*
 interface Uxzser {
     lastName: string;
 }
 */
-// let myUser = NumOrSrt;
+
+//let myUser = NumOrSrt;
+
 /*
 let objUser: User = { name: 'Anton' };
 objUser.name = 'test';
 */
+
+
 /*
 function myFunction(str1: string): number  {
     return 0;
