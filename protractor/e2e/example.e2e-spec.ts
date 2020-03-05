16b87a9825ce60837a8995bc36b22f2aaf5a8ceb
neberaa <neberaanton@gmail.com>
2020-03-05 23:43:06 +0200

    play with simple test

--- protractor/e2e/example.e2e-spec.ts
+++ protractor/e2e/example.e2e-spec.ts
@@ -1,13 +1,41 @@
-import {browser} from 'protractor';
+import {browser, $, element, by} from 'protractor';
 
-describe('Test', function () {
-    it('should passed', async () => {
-        await browser.get('/');
-        expect(true).toBe(true);
+describe('Demo Calculator Test', function () {
+    beforeAll(() => {
+        browser.waitForAngularEnabled(false);
     });
+    beforeEach(() => {
+        browser.get('http://juliemr.github.io/protractor-demo/');
+    });
+    it('check title',  () => {
+        expect(browser.getTitle()).toEqual('Super Calculator');
+    });
+    it('fill inputs', () => {
+       const firstInput = element(by.model('first'));
+       const secondInput = element(by.model('second'));
+       firstInput.sendKeys(5);
+       secondInput.sendKeys(3);
+    });
+    it('button click',  () => {
+       const button = $('#gobutton');
+       button.click();
+    });
+    it('check the result', () => {
+       const result = $('h2.ng-binding');
+       expect(result.getText()).toEqual(8);
+    });
+    // it('should passed', async () => {
+    //     const searchField = $('[name="q"]');
+    //     const submitButton = $('center [value="Google search"]');
+    //
+    //     await browser.get('/');
+    //     searchField.sendKeys('hello world');
+    //     browser.sleep(5000);
+    //     expect(true).toBe(true);
+    // });
 
-    it('should failed', async () => {
-        await browser.get('/');
-        expect(true).toBe(false);
-    });
+    // it('should failed', async () => {
+    //     await browser.get('/');
+    //     expect(true).toBe(false);
+    // });
 });
