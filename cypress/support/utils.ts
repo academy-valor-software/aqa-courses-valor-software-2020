3b7cdfb54821769b20f4581b114d94ad1e915044
neberaa <neberaanton@gmail.com>
2020-03-19 22:38:18 +0200

    HW#4: Cypress e2e tests: - previous lesson clean up

--- cypress/support/utils.ts
+++ cypress/support/utils.ts
@@ -1,15 +1,3 @@
-export function getRandom(max: number): number {
-    return Math.round(Math.random() * max);
-}
-
-export function getSubstringByRegex(text: string, regex: string): string {
-    const result = text.match(regex) || [];
-    if (result[1]) {
-        return result[1];
-    }
-    return 'Nothing found';
-}
-
 export function getRandomString(): string {
     return Math.random().toString(36).slice(-10) + '@';
 }
